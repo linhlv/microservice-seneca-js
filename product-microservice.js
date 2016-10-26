006e5bbe8f657e34afe1498a5bedc23fecb4c2c9
Linh Le <dev.linhlv@gmail.com>
2016-10-26 11:38:16 +0700

    Extend functionality

--- product-microservice.js
+++ product-microservice.js
@@ -9,6 +9,23 @@ seneca.add({role:'math', cmd: 'product'}, function onMatch(msg, respond){
   respond(null, {answer: product});
 });
 
+/*
+seneca.add({role: 'math', cmd: 'sum', integer: true},
+  function(msg, respond){
+    var sum = Math.floor(msg.left) + Math.floor(msg.right);
+    respond(null, {answer: sum});
+});
+*/
+
+seneca.add('role:math, cmd:sum, integer: true', function(msg, respond){
+  this.act({
+    role: 'math',
+    cmd: 'sum',
+    left: Math.floor(msg.left),
+    right: Math.floor(msg.right)
+  }, respond);
+});
+
 seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, function(err, result){
   if(err) return console.error(err);
   console.log(result);
